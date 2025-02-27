a7f0dc16852538b0b1d475c7931c6f162b61d7fb
FlutterCommitte <minders.flutter@gmail.com>
2025-02-27 13:49:41 +0200

    Create Home Cubits

---
+++ lib/Fetures/Home/ViewModel/cubit/home_screen_cubit_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:bloc/bloc.dart';
+part 'home_screen_cubit_state.dart';
+
+class HomeScreenCubitCubit extends Cubit<HomeScreenCubitState> {
+  
+  HomeScreenCubitCubit() : super(HomeScreenCubitSuccsses());
+}
---
+++ lib/Fetures/Home/ViewModel/cubit/home_screen_cubit_state.dart
@@ -0,0 +1,11 @@
+part of 'home_screen_cubit_cubit.dart';
+
+abstract class HomeScreenCubitState {}
+
+final class HomeScreenCubitInitial extends HomeScreenCubitState {}
+
+final class HomeScreenCubitLoading extends HomeScreenCubitState {}
+
+final class HomeScreenCubitSuccsses extends HomeScreenCubitState {}
+
+final class HomeScreenCubitFaluir extends HomeScreenCubitState {}
