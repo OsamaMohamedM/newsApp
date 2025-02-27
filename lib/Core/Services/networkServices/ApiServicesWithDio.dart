b1a86215ed8da4d4d591fadd4c29ff546e7a0719
FlutterCommitte <minders.flutter@gmail.com>
2025-02-27 20:18:19 +0200

    Home is done

--- lib/Core/Services/networkServices/ApiServicesWithDio.dart
+++ lib/Core/Services/networkServices/ApiServicesWithDio.dart
@@ -3,17 +3,11 @@ import 'package:newsapp/Core/Services/networkServices/ApiServices.dart';
 class ApiserviceswithdioImp extends ApiServices {
   ApiserviceswithdioImp(super.dio);
   @override
-  Future<Map<String, dynamic>> fetchDataUs(String endPoint) async {
+  Future<Map<String, dynamic>> fetchDatawithUs(String endPoint) async {
     var result =
         await dio.get('$baseURL$endPoint&sortBy=popularity&apiKey=$apiKey');
 
     return result.data;
   }
-
-  Future<Map<String, dynamic>> fetchDatawithCategory(String endPoint) async {
-    var result =
-        await dio.get('$baseURL$endPoint&sortBy=popularity&apiKey=$apiKey');
-
-    return result.data;
-  }
+  
 }
